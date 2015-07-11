90db196cab28bfd67094a0b4922a86e400c31d77
Kiko Beats <josefrancisco.verdu@gmail.com>
2015-07-11 15:48:17 +0200

    deleted unnecessary code

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
-require('coffee-script/register')
-// var ColorizeBoolean =
-module.exports = require('./lib')
+var chalk = require('chalk');
+
+module.exports = function(_keyword) {
+  return {
+    expected: function(_expected) {
+      return {
+        actual: function(_actual) {
+          var color;
+          _keyword || (_keyword = _expected);
+          if (process.env.NODE_ENV === 'production') return _keyword;
+          color = _expected === _actual ? 'green' : 'red';
+          return chalk[color](_keyword);
+        }
+      };
+    }
+  };
+};
