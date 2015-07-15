22c1dc11440ef3ae75e5d8ed1d2c2c9b2e5b30e8
Kiko Beats <josefrancisco.verdu@gmail.com>
2015-07-15 16:52:06 +0200

    fixed the value to colorize

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 'use strict';
 var chalk = require('chalk');
 
-module.exports = function(_keyword) {
+module.exports = function(keyword) {
   return {
-    expected: function(_expected) {
+    expected: function(expected) {
       return {
-        actual: function(_actual) {
+        actual: function(actual) {
           var color;
-          _keyword || (_keyword = _expected);
-          if (process.env.NODE_ENV === 'production') return _keyword;
-          color = _expected === _actual ? 'green' : 'red';
-          return chalk[color](_keyword);
+          var value;
+
+          if (expected === actual) {
+            value = expected;
+            color = 'green';
+          } else {
+            value = actual;
+            color = 'red';
+          }
+
+          value = keyword || value;
+
+          if (process.env.NODE_ENV === 'production') return value;
+          return chalk[color](value);
         }
       };
     }
